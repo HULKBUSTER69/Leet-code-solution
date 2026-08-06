74ea9da674a3ffce706ebb7f58c59d3f4ea29e8a
Gagan-Chaurasia <xyz77434@gmail.com>
2026-08-07 03:20:58 +0530

    Time: 6 ms (16.56%) | Memory: 45.6 MB (13.25%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -4,11 +4,12 @@ class Solution {
         Arrays.sort(nums);
         int end= nums.length-1;
         int i=nums[0];
-        while(i <= nums[0])
-        {
+             while(i <= nums[0])
+            {
             if( nums[0]%i==0 && nums[end]%i==0) return i;
             else i--;
-        }
+            }
+        
         return 1;
     }
 }
